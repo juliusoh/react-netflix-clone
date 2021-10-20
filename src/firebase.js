b5ee86e12af7afcfdd5fed5c4c7609d82c0d05cd
julius oh <juliusoh@gmail.com>
2021-10-19 22:57:04 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCDUSArXAMfEQYmTQRGPqUx18YL0yj5EMs",
+  authDomain: "netflix-clone-5d88d.firebaseapp.com",
+  projectId: "netflix-clone-5d88d",
+  storageBucket: "netflix-clone-5d88d.appspot.com",
+  messagingSenderId: "55290435543",
+  appId: "1:55290435543:web:4a78e7bda569d550421d92"
+};
